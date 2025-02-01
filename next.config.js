02f981f10bf3e8f76631dea4ffccb95fbe461d28
louanne <lecocqpro1@gmail.com>
2025-02-01 14:53:27 +0100

    add production settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
     },
-    basePath: '/portfolio',
-    }
+    basePath: isProduction ? '/portfolio' : "",
+}
 
 module.exports = nextConfig
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
-import '@fontsource/geist';
+import { Inter } from 'next/font/google';
 import './globals.scss';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Lou-Anne Lecocq - Portfolio',
@@ -9,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className="font-geist">{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

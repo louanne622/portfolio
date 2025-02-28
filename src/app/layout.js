699189a1f0cac8e599d4d0ec200fea66cc409523
louanne <lecocqpro1@gmail.com>
2025-02-28 10:40:15 +0100

    remove build github pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from 'next/font/google';
 import "./styles/global.scss";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const geist = Geist({
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata = {
@@ -19,9 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geist.className} antialiased`}>
         {children}
       </body>
     </html>
